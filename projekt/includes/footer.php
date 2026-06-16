082646fe06f584bae374cd3726a8cbc35d428131
patrik-balija <pbalija@tvz.hr>
2026-06-16 18:59:26 +0200

    Delete projekt directory

--- projekt/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-container">
-        <p>&copy; <?php echo date("Y"); ?> Exclusivia d.o.o. Sva prava pridržana.</p>
-    </div>
-</footer>
-</body>
-</html>
--- projekt/views/contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once __DIR__ . "/../includes/header.php";
-?>
-
-<h1><?php echo $page; ?></h1>
-
-<?php
-    include_once __DIR__ . "/../includes/footer.php";
-?>
